91fe4740d0b3e7db00a0634d1f2e90efad358aa5
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-12-05 19:46:54 +0300

    fx

---
+++ node_js_it-ink/learn/scipt.js
@@ -0,0 +1,63 @@
+//____Simple express app with typescript and nodemon___//
+
+// yarn init --yes
+// yarn add express
+
+//index.js
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  debugger
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+// node index.js
+//автоматические видить изменения -  yarn add nodemon --dev(для разработки)
+//yarn nodemon index.js
+
+//для инспекции кода добовляем --inspect / далее иконка в консоли node
+//yarn nodemon --inspect index.js
+
+//добовляем ts
+//yarn add typescript ts-node @types/node @types/express --dev
+
+//создать дефолтный файл с настройками ts
+//yarn tsc --init - создать файл конфигурационный ts
+
+//компиляция yarn tsc чтобы получить итоговый файл в js
+//tsconfig.json
+//"rootDir": "./src"  - исходный файд
+//"outDir": "./dist" - скомпилированный файл
+
+//режим watcher для ts в реальном времени следит за файлами ts
+//yarn tsc -w
+
+//то есть одним компилятором следим за ts a nodemon смотрит уже за dist/index.js
+
+//пишем скрипты для упрощения запуска проги
+/* 
+ "scripts": {
+    "watch": "tsc -w",
+    "dev": "nodemon --inspect dist/index.js"
+  },
+*/
+//запускаем на разных консолях
+
+//---------------------------------------------------------
+//__02 - 1 - Deploy to Heroku for simple TS Express App__//
+//Heroku - облочный сервер
+//Связываем github и heroku
+
+/* 
+"scripts": {
+    "watch": "tsc -w",
+    "dev": "nodemon --inspect dist/index.js",
+    "build":"tsc", -> для компиляиции на heroku без -w
+    "start":"node dist/index.js" -> для компиляиции на heroku без nodemon
+  },
+*/
