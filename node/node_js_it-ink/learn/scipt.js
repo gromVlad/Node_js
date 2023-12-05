bad153bf027fbcfc3c88f63c9e1eeb166de43927
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-12-05 19:09:28 +0300

    it continue

--- node/node_js_it-ink/learn/scipt.js
+++ node/node_js_it-ink/learn/scipt.js
@@ -22,3 +22,31 @@ app.listen(port, () => {
 
 //для инспекции кода добовляем --inspect / далее иконка в консоли node
 //yarn nodemon --inspect index.js
+
+//добовляем ts
+//yarn add typescript ts-node @types/node @types/express --dev
+
+//создать дефолтный файл с настройками ts
+//yarn tsc --init - создать файл конфигурационный ts
+
+//компиляция yarn tsc чтобы получить итоговый файл в js
+//tsconfig.json
+//"rootDir": "./src"  - исходный файд
+//"outDir": "./dist" - скомпилированный файл
+
+//режим watcher для ts в реальном времени следит за файлами ts
+//yarn tsc -w
+
+//то есть одним компилятором следим за ts a nodemon смотрит уже за dist/index.js
+
+//пишем скрипты для упрощения запуска проги
+/* 
+ "scripts": {
+    "watch": "tsc -w",
+    "dev": "nodemon --inspect dist/index.js"
+  },
+*/
+//запускаем на разных консолях
+
+//------------------------------------
+//_________
