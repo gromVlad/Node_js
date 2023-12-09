2859645a78be579745fb1589e814237cb6fd295a
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-12-09 14:37:22 +0300

    fx

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express()
-const port = process.env.POPT || 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req:any, res:any) => {
   res.send('Hello!')
