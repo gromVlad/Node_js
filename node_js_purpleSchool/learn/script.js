267a511af5d3c60fd60729cb09027c95130892ae
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-12-17 17:13:27 +0300

    countinue purpleSC

--- node_js_purpleSchool/learn/script.js
+++ node_js_purpleSchool/learn/script.js
@@ -660,7 +660,11 @@ export { printError, printSuccess, printHelp, printWeather };
 //_____OS и path____//
 
 //storage.service.js
+
+//работа с нашей дерикторией ос
 import { homedir } from 'os';
+
+//работа с путями
 import { join, basename, dirname, extname, relative, isAbsolute,resolve,sep } from 'path';
 
 const filePath = join(homedir(), 'weather-data.json');
@@ -681,5 +685,49 @@ saveKeyvalue();
 
 //------------------------------------
 //_____Работа с файловой системой__//
+import { homedir } from 'os';
+import { join } from 'path';
+import { promises } from 'fs';
+
+const filePath = join(homedir(), 'weather-data.json');
+
+const TOKEN_DICTIONARY = {
+  token: 'token',
+  city: 'city'
+}
+
+const saveKeyValue = async (key, value) => {
+  let data = {};
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    data = JSON.parse(file);
+  }
+  data[key] = value;
+  await promises.writeFile(filePath, JSON.stringify(data));
+};
+
+const getKeyValue = async (key) => {
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    const data = JSON.parse(file);
+    return data[key];
+  }
+  return undefined;
+};
+
+const isExist = async (path) => {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+export { saveKeyValue, getKeyValue, TOKEN_DICTIONARY };
+
+//--------------------------------
+//___Взаимодействие с API___//
+//используем API wether
 
 
